53e001faae08d9efc59a36192483a95bcb7a5c4a
liraCinthia <cliravieira@gmail.com>
2024-07-27 11:52:01 -0300

    refactor

--- include/push_swap.h
+++ include/push_swap.h
@@ -23,6 +23,9 @@ int ft_just_numbers(char *str);
 int ft_strcmp(const char *s1, const char *s2);
 int ft_has_sign(char c);
 int ft_support_repeated_numbers(const char *s1, const char *s2);
+int ft_repeated_numbers(char **argv);
+int ft_input_checks(char **argv);
+int ft_is_zero(char *argv);
 
 
 #endif
