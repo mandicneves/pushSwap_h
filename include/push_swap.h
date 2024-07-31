8326eb64e8b7d717889d208500fa008b9b65897e
liraCinthia <cliravieira@gmail.com>
2024-07-31 17:58:15 -0300

    refactor

--- include/push_swap.h
+++ include/push_swap.h
@@ -35,6 +35,7 @@ void    ft_have_target_position(t_stack **a_stack, t_stack **b_stack);
 void    ft_push(t_stack **src, t_stack **dest);
 void    ft_make_push_a(t_stack **a_stack, t_stack **b_stack);
 void    ft_make_push_b(t_stack **a_stack, t_stack **b_stack);
+void    ft_rotate(t_stack **stack);
 
 
 #endif
