c51e08cb79aa8c512b3a0f122fe3e6ea3df589c9
liraCinthia <cliravieira@gmail.com>
2024-07-31 16:38:54 -0300

    add function

--- sources/position.c
+++ sources/position.c
@@ -14,3 +14,26 @@ void    ft_have_position(t_stack **stack)
         i++;
     }
 }
+
+void    ft_have_target_position(t_stack **a_stack, t_stack **b_stack)
+{
+    t_stack *a_node;
+    t_stack *b_node;
+
+    b_node = *b_stack;
+    while (b_node)
+    {
+        a_node = *a_stack;
+        b_node->target_position = INT_MAX;
+        while (a_node)
+        {
+            if (b_node->index < a_node->index
+                && a_node->index < b_node->target_position)
+                {
+                    b_node->target_position = a_node->index;
+                }
+                a_node = a_node->next;
+        }
+        b_node = b_node->next;
+    }
+}
