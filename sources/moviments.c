988a72c1506c02f1d52fd0ff9a9901e5a4c5af82
liraCinthia <cliravieira@gmail.com>
2024-08-01 00:22:09 -0300

    refactor

--- sources/moviments.c
+++ sources/moviments.c
@@ -2,86 +2,86 @@
 
 void    ft_rotate_rr(t_stack **a_stack, t_stack **b_stack, int a, int b)
 {
-    t_stack *node;
+    t_stack *current_node;
 
-    node = *a_stack;
-    while (node && node->index != a)
-        node = node->next;
-    node->a_cost--;
-    node = *b_stack;
-    while (node && node->index != b)
-        node = node->next;
-    node->b_cost--;
+    current_node = *a_stack;
+    while (current_node && current_node->index != a)
+        current_node = current_node->next;
+    current_node->a_cost--;
+    current_node = *b_stack;
+    while (current_node && current_node->index != b)
+        current_node = current_node->next;
+    current_node->b_cost--;
     ft_make_rotate_a_and_b(a_stack, b_stack);
 }
 
 void    ft_reverse_rotate_rrr(t_stack **a_stack, t_stack **b_stack, int a, int b)
 {
-    t_stack *node;
+    t_stack *current_node;
 
-    node = *a_stack;
-    while (node && node->index != a)
-        node = node->next;
-    node->a_cost++;
-    node = *b_stack;
-    while (node && node->index != b)
-        node = node->next;
-    node->b_cost++;
-    ft_make_reverse_rotate_a_and_b                                                          (a_stack, b_stack);
+    current_node = *a_stack;
+    while (current_node && current_node->index != a)
+        current_node = current_node->next;
+    current_node->a_cost++;
+    current_node = *b_stack;
+    while (current_node && current_node->index != b)
+        current_node = current_node->next;
+    current_node->b_cost++;
+    ft_make_reverse_rotate_a_and_b(a_stack, b_stack);
 }
 
 void    ft_move_stack_a(t_stack **a_stack, int curret_pos)
 {
-    t_stack *node;
+    t_stack *current_node;
 
-    node = *a_stack;
-    while (node)
+    current_node = *a_stack;
+    while (current_node)
     {
-        if (node->index == curret_pos)
+        if (current_node->index == curret_pos)
         {
-            while (node->a_cost != 0)
+            while (current_node->a_cost != 0)
             {
-                if (node->a_cost > 0)
+                if (current_node->a_cost > 0)
                 {
                     ft_make_rotate_a(a_stack);
-                    node->a_cost--;
+                    current_node->a_cost--;
                 }
-                if (node->a_cost < 0)
+                if (current_node->a_cost < 0)
                 {
                     ft_make_reverse_rotate_a(a_stack);
-                    node->a_cost++;
+                    current_node->a_cost++;
                 }
             }
             return ;
         }
-        node = node->next;
+        current_node = current_node->next;
     }
 }
 
 void    ft_move_stack_b(t_stack **b_stack, int current_pos)
 {
-    t_stack *node;
+    t_stack *current_node;
 
-    node = *b_stack;
-    while (node)
+    current_node = *b_stack;
+    while (current_node)
     {
-        if (node->position == current_pos)
+        if (current_node->position == current_pos)
         {
-            while (node->b_cost != 0)
+            while (current_node->b_cost != 0)
             {
-                if (node->b_cost > 0)
+                if (current_node->b_cost > 0)
                 {
                     ft_make_rotate_b(b_stack);
-                    node->b_cost--;
+                    current_node->b_cost--;
                 }
-                if (node->b_cost < 0)
+                if (current_node->b_cost < 0)
                 {
                     ft_make_reverse_rotate_b(b_stack);
-                    node->b_cost++;
+                    current_node->b_cost++;
                 }
             }
             return ;
         }
-        node = node->next;
+        current_node = current_node->next;
     }
 }
