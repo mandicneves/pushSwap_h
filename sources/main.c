ccbae8bb5bdb0de32d3d280bd96fc5fcfc647dec
liraCinthia <cliravieira@gmail.com>
2024-08-01 11:49:39 -0300

    finish

--- sources/main.c
+++ sources/main.c
@@ -21,3 +21,27 @@ void    ft_push_swap(t_stack **a_stack, t_stack **b_stack, int stack_size)
     else if (stack_size > 3)
         ft_sort(a_stack, b_stack);
 }
+
+int main(int argc, char **argv)
+{
+    t_stack *a_stack;
+    t_stack *b_stack;
+    int stack_size;
+
+    b_stack = NULL;
+    if (argc == 1)
+        return (1);
+    if (ft_input_checks(argv) == 1)
+    {
+        ft_putstr_fd("Error\n", 2);
+        exit(1);
+    }
+    a_stack = ft_initial_stack(argc, argv);
+    stack_size = ft_get_stack_size(a_stack);
+    ft_assigning_indexes(a_stack);
+    if (ft_is_sorted(a_stack) == 1)
+        ft_push_swap(&a_stack, &b_stack, stack_size);
+    ft_free_stack(&a_stack);
+    ft_free_stack(&b_stack);
+    return (0);
+}
