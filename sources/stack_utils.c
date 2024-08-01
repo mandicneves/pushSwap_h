520b5d00056cc105259538b77705b938b25d13cd
liraCinthia <cliravieira@gmail.com>
2024-07-31 22:58:28 -0300

    add function

--- sources/stack_utils.c
+++ sources/stack_utils.c
@@ -14,3 +14,21 @@ int ft_get_stack_size(t_stack *stack)
     }
     return (size);
 }
+
+t_stack *ft_add_node(int nbr)
+{
+    t_stack *new_node;
+
+    new_node = malloc(sizeof * new_node);
+    if (!new_node)
+        return (NULL);
+    new_node->value = nbr;
+    new_node->index = 1;
+    new_node->position = 0;
+    new_node->target_position = 0;
+    new_node->a_cost = 0;
+    new_node->b_cost = 0;
+    new_node->all_cost = 0;
+    new_node->next = NULL;
+    return (new_node);
+}
