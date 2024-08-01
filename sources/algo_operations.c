9d1bd72c330f05c293af5143520f54d3fa83422d
liraCinthia <cliravieira@gmail.com>
2024-08-01 11:07:11 -0300

    add function

--- sources/algo_operations.c
+++ sources/algo_operations.c
@@ -17,3 +17,28 @@ void    ft_get_best_operation(t_stack **a_stack, t_stack **b_stack)
         current_node = current_node->next;
     }
 }
+
+void    ft_moviments(t_stack **a_stack, t_stack **b_stack, int target_position, int position)
+{
+    t_stack *a_current;
+    t_stack *b_current;
+
+    a_current = *a_stack;
+    b_current = *b_stack;
+    while (a_current->index != target_position)
+        a_current = a_current->next;
+    while (b_current->position != position)
+        b_current = b_current->next;
+    while (b_current)
+    {
+        if (a_current->a_cost > 0 && b_current->b_cost > 0)
+            ft_rotate_rr(a_stack, b_stack, a_current->index, b_current->index);
+        else if (a_current->a_cost < 0 && b_current->a_cost < 0)
+            ft_reverse_rotate_rrr(a_stack, b_stack, a_current->index, a_current->index);
+        ft_move_stack_a(a_stack, target_position);
+        ft_move_stack_b(b_stack, position);
+        if (a_current->a_cost == 0 && b_current->b_cost == 0)
+            break;
+    }
+    ft_make_push_a(a_stack, b_stack);
+}
