7b7f6978f6471d6ce2858eb7a31fda4417d711f7
liraCinthia <cliravieira@gmail.com>
2024-08-01 00:04:08 -0300

    refactor

--- sources/position.c
+++ sources/position.c
@@ -17,23 +17,23 @@ void    ft_have_position(t_stack **stack)
 
 void    ft_have_target_position(t_stack **a_stack, t_stack **b_stack)
 {
-    t_stack *a_node;
-    t_stack *b_node;
+    t_stack *current_a_node;
+    t_stack *current_b_node;
 
-    b_node = *b_stack;
-    while (b_node)
+    current_b_node = *b_stack;
+    while (current_b_node)
     {
-        a_node = *a_stack;
-        b_node->target_position = INT_MAX;
-        while (a_node)
+        current_a_node = *a_stack;
+        current_b_node->target_position = INT_MAX;
+        while (current_a_node)
         {
-            if (b_node->index < a_node->index
-                && a_node->index < b_node->target_position)
+            if (current_b_node->index < current_a_node->index
+                && current_a_node->index < current_b_node->target_position)
                 {
-                    b_node->target_position = a_node->index;
+                    current_b_node->target_position = current_a_node->index;
                 }
-                a_node = a_node->next;
+                current_a_node = current_a_node->next;
         }
-        b_node = b_node->next;
+        current_b_node = current_b_node->next;
     }
 }
