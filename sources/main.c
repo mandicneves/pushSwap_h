5403cd9094d9fe5d0b6a8bb6f3615559038f9031
liraCinthia <cliravieira@gmail.com>
2024-08-01 11:43:53 -0300

    add function

--- sources/main.c
+++ sources/main.c
@@ -10,3 +10,14 @@ int ft_is_sorted(t_stack *stack)
     }
     return (0);
 }
+
+void    ft_push_swap(t_stack **a_stack, t_stack **b_stack, int stack_size)
+{
+    *b_stack = NULL;
+    if (stack_size == 2)
+        ft_make_swap_a(a_stack);
+    else if (stack_size == 3)
+        ft_sort_small(a_stack);
+    else if (stack_size > 3)
+        ft_sort(a_stack, b_stack);
+}
