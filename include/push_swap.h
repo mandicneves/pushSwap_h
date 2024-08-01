a54ec4e9fd7fd3063129e4bf6c87143e3aaa6493
liraCinthia <cliravieira@gmail.com>
2024-07-31 22:40:58 -0300

    refactor

--- include/push_swap.h
+++ include/push_swap.h
@@ -49,6 +49,7 @@ void    ft_make_swap_b(t_stack **b_stack);
 void    ft_make_swap_a_and_b(t_stack **a_stack, t_stack **b_stack);
 long int    ft_atol(const char *nptr);
 int ft_sum_cost(int a, int b);
+void    ft_free_stack(t_stack **stack);
 
 
 #endif
