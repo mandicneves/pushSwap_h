419f1a07c40d960dd635d6090fa22b9e301044da
liraCinthia <cliravieira@gmail.com>
2024-08-01 11:23:17 -0300

    add function

--- sources/sort.c
+++ sources/sort.c
@@ -36,3 +36,28 @@ void    ft_sort(t_stack **a_stack, t_stack **b_stack)
     if (ft_is_sorted(*a_stack) == 1)
         ft_sorting_stack_a(a_stack);
 }
+
+void    ft_sorting_stack_a(t_stack **a_stack)
+{
+    int stack_size;
+    int low_position;
+
+    stack_size = ft_get_stack_size(*a_stack);
+    low_position = ft_lowest_position(*a_stack);
+    if (low_position > stack_size / 2)
+    {
+        while (low_position < stack_size)
+        {
+            ft_make_reverse_rotate_a(a_stack);
+            low_position++;
+        }
+    }
+    else
+    {
+        while (low_position != 0)
+        {
+            ft_make_rotate_a(a_stack);
+            low_position--;
+        }
+    }
+}
